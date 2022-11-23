663aecba62543db8186a72eb2e7229f64386029c
danaremar <56022332+danaremar@users.noreply.github.com>
2022-11-23 19:40:00 +0100

    Sincronized _id in MongoDB
    Adapted _id with MongoDB

--- match_service/match_api/models.py
+++ match_service/match_api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 # Create your models here.
 class Match(models.Model):
+    id = models.CharField(primary_key=True, max_length=24, default=get_random_string(length=24))
     local = models.CharField(max_length=255)
     visitor = models.CharField(max_length=255)
     alignment = models.CharField(max_length=255)
