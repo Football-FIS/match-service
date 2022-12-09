14550eefa838bb57efe244b072c735ba9d5b8548
danaremar <56022332+danaremar@users.noreply.github.com>
2022-12-09 18:33:42 +0100

    Config SwaggerUI
    Configured SwaggerUI, match lcrud fixed & update README.

--- match_service/match_api/urls.py
+++ match_service/match_api/urls.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Match Service API",
+      default_version='v1',
+      description="It's a microservice from footmatch app, a project from University of Seville.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="trabajofis2022@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('match/', views.MatchListApiView.as_view()),
-    path('match/new', views.MatchCreateApiView.as_view()),
-    path('match/detail/<pk>', views.MacthRetrieveApiView.as_view()),
-    path('match/delete/<pk>', views.MatchDestroyApiView.as_view()),
-    path('match/update/<pk>', views.MatchUpdateApiView.as_view()),
+    path('match/list', views.MatchViewSet.as_view({'get':'list'})),
+    path('match/', views.MatchViewSet.as_view({'post':'create'})),
+    path('match/<pk>', views.MatchViewSet.as_view({'get':'get', 'put':'update', 'delete':'delete'})),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- match_service/match_api/views.py
+++ match_service/match_api/views.py
@@ -1,34 +1,47 @@
-from django.shortcuts import render
-from rest_framework.generics import (
-    ListAPIView,
-    CreateAPIView,
-    RetrieveAPIView,
-    DestroyAPIView,
-    RetrieveUpdateAPIView
-    )
 from .serializers import MatchSerializer
 from .models import Match
-# Create your views here.
+from rest_framework import viewsets, status
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-
-class MatchListApiView(ListAPIView):
+class MatchViewSet(viewsets.ModelViewSet):
 
     serializer_class = MatchSerializer
 
-    def get_queryset(self):
-        return Match.objects.all()
+    def get_object(self, pk):
+        queryset = Match.objects.all()
+        return get_object_or_404(queryset, pk=pk)
 
-class MatchCreateApiView(CreateAPIView):
-    serializer_class = MatchSerializer
+    # list
+    def list(self, request):
+        queryset = Match.objects.all()
+        serializer_class = MatchSerializer(queryset, many=True)
+        return Response(serializer_class.data)
 
-class MacthRetrieveApiView(RetrieveAPIView):
-    serializer_class = MatchSerializer
-    queryset = Match.objects.filter()
+    # get
+    def get(self, request, pk=None):
+        match = self.get_object(pk)
+        serializer_output = MatchSerializer(match)
+        return Response(serializer_output.data)
 
-class MatchDestroyApiView(DestroyAPIView):
-    serializer_class = MatchSerializer
-    queryset = Match.objects.all()
+    # create
+    def create(self, request):
+        serializer = MatchSerializer(data=request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class MatchUpdateApiView(RetrieveUpdateAPIView):
-    serializer_class = MatchSerializer
-    queryset = Match.objects.all()
+    # update
+    def update(self, request, pk):
+        match = self.get_object(pk)
+        serializer = MatchSerializer(data=request.data)
+        serializer.is_valid()
+        serializer.update(match, request.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # delete
+    def delete(self, request, pk):
+        match = self.get_object(pk)
+        match.delete()
+        return Response(status=status.HTTP_200_OK)
+        
